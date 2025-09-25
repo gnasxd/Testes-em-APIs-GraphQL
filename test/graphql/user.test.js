aff787341f58aa4d95a44fe9fa013304887ade77
Guilherme Augusto <guibiston@gmail.com>
2025-09-25 19:19:27 -0300

    modi

--- test/graphql/user.test.js
+++ test/graphql/user.test.js
@@ -1,10 +1,32 @@
 // test.js
 const { spec } = require('pactum');
-const {like, eachLike} = require ('pactum-matchers');
+const { like, eachLike } = require('pactum-matchers');
+
+let token;
+beforeEach(async () => {
+    token = await spec()
+        .post('http://lojaebac.ebaconline.art.br/graphql')
+        .withGraphQLQuery(`
+            mutation AuthUser($email: String, $password: String) {
+  authUser(email: $email, password: $password) {
+    success
+    token
+
+  }
+}
+
+  `)
+        .withGraphQLVariables({
+            "email": "admin@admin.com",
+            "password": "admin123"
+        })
+         .stores('token', 'data.authUser.token')
+});
 
 it('listagem de usuarios', async () => {
     await spec()
         .post('http://lojaebac.ebaconline.art.br/graphql')
+        .withHeaders("Authorization", "$S{token}")
         .withGraphQLQuery(`
    query Users {
   Users {
@@ -17,17 +39,17 @@ it('listagem de usuarios', async () => {
 }
   `)
         .expectStatus(200)
-         .expectJsonMatch({
+        .expectJsonMatch({
             data: {
                 Users: eachLike({
-                    id: like ("67ad03316f0ec268a020e40e"),
-                    email: like ("cliente@email.com"),
+                    id: like("67ad03316f0ec268a020e40e"),
+                    email: like("cliente@email.com"),
                     profile: {
-                      firstName: like("Cliente")
+                        firstName: v => v === null || typeof v === 'Cliente'
                     }
 
 
                 })
-            } 
-  });
+            }
+        });
 });
