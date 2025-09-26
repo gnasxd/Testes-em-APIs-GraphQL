3097060d9e1e4108879e8b6d3240344b8282a844
Guilherme Augusto <guibiston@gmail.com>
2025-09-25 23:17:03 -0300

    modi

--- test/graphql/login.test.js
+++ test/graphql/login.test.js
@@ -2,22 +2,20 @@
 const { spec } = require('pactum');
 
 it('deve autenticar usuarios corretamente', async () => {
-    await spec()
-        .post('http://lojaebac.ebaconline.art.br/graphql')
-        .withGraphQLQuery(`
-            mutation AuthUser($email: String, $password: String) {
-  authUser(email: $email, password: $password) {
-    success
-    token
-
-  }
-}
-
-  `)
-        .withGraphQLVariables({
-            "email": "admin@admin.com",
-            "password": "admin123"
-        })
-        .expectStatus(200)
-       .expectJson('data.authUser.success', true)
+  await spec()
+    .post('http://lojaebac.ebaconline.art.br/graphql')
+    .withGraphQLQuery(`
+      mutation AuthUser($email: String, $password: String) {
+        authUser(email: $email, password: $password) {
+          success
+          token
+        }
+      }
+    `)
+    .withGraphQLVariables({
+      email: "admin@admin.com",
+      password: "admin123"
+    })
+    .expectStatus(200)
+    .expectJson('data.authUser.success', true);
 });
